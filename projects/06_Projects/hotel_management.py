c545ace4a396c01737351217621bb703def1fba1
chickennuggetduck33 <107875096+chickennuggetduck33@users.noreply.github.com>
2024-10-27 19:57:29 +0000

    skibidi toilet wil be mine, yeah.  ohio town, yeah.

--- projects/06_Projects/hotel_management.py
+++ projects/06_Projects/hotel_management.py
@@ -1,8 +1,16 @@
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 
 rooms = {
-    '1':''
+    1:'',
+    2:'',
+    3:''
 }
 
-def check_in_or_out:
-    
+def check_in_or_out():
+    checkin = simpledialog.askstring(None, prompt="would you like to check in or out a quest?")
+    if checkin == "check in":
+       pass
+    elif checkin == "check out":
+        pass
+    else:
+        messagebox.showinfo(None, message="try saying check in or check out")
