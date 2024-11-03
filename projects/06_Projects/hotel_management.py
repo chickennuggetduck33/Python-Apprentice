2856f8963b7541c08070ed3a404aa4439fd8c24b
chickennuggetduck33 <107875096+chickennuggetduck33@users.noreply.github.com>
2024-11-03 20:58:54 +0000

    you cant rizz up my sigma gyatt

--- projects/06_Projects/hotel_management.py
+++ projects/06_Projects/hotel_management.py
@@ -3,14 +3,31 @@ from tkinter import simpledialog, messagebox
 rooms = {
     1:'',
     2:'',
-    3:''
+    3:'',
+    4:'',
+    5:''
 }
-
+running = True
 def check_in_or_out():
     checkin = simpledialog.askstring(None, prompt="would you like to check in or out a quest?")
+    print(checkin)
     if checkin == "check in":
-       pass
+       roomnumber = simpledialog.askinteger(None, prompt="what room number?")
+       if rooms[roomnumber] == '':
+           messagebox.showinfo(None, message="room "+ str(roomnumber)+" is open")
+           name = simpledialog.askstring(None, prompt="who would you like to check in?")
+           rooms[roomnumber] = name
+       else:
+           messagebox.showinfo(None, message="room "+str(roomnumber) +" is already reserved")
     elif checkin == "check out":
         pass
+    elif checkin == "exit":
+        print("HERE")
+        running = False
+        return False
     else:
         messagebox.showinfo(None, message="try saying check in or check out")
+
+while running:
+    if check_in_or_out() == False:
+        running = False
